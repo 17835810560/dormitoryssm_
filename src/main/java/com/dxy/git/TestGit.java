a1a899ab66cd30c8789b413ef4a73af36f77a5e8
dxy <dxy@qq.com>
2024-11-15 11:04:02 +0800

    first1 commit

---
+++ src/main/java/com/dxy/git/TestGit.java
@@ -0,0 +1,12 @@
+package com.dxy.git;
+
+/**
+ * @author 杜老板
+ * @Version 1.0
+ */
+public class TestGit {
+    public static void main(String[] args) {
+        System.out.println("hello ,git!!");
+       // System.out.println("hello222");
+    }
+}
