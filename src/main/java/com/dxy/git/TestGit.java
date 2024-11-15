6259d4c2671f39e41126fe858147c874cfbc64f9
dxy <dxy@qq.com>
2024-11-15 11:05:58 +0800

    second commit

--- src/main/java/com/dxy/git/TestGit.java
+++ src/main/java/com/dxy/git/TestGit.java
@@ -7,6 +7,6 @@ package com.dxy.git;
 public class TestGit {
     public static void main(String[] args) {
         System.out.println("hello ,git!!");
-       // System.out.println("hello222");
+        System.out.println("hello222");
     }
 }
